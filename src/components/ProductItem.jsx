501079d7e822fc184965db07ce17b5833d4fc7db
adityasusawat <akablackdevils300@gmail.com>
2024-03-26 12:07:21 +0530

    image fetch and display + env

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -5,10 +5,24 @@ import { useEffect, useState } from "react";
 export default function ProductItem({ product }) {
   const { id, available, colour, images, productName, size } = product;
 
+  let imageLinks = images.map((img) => {
+    return `${import.meta.env.VITE_FILES_URL}/${product.collectionId}/${
+      product.id
+    }/${img}`;
+  });
+
+  console.log(imageLinks);
+
   return (
-    <div>
+    <div style={{ border: "1px solid black" }}>
       <h3>{productName}</h3>
       <p>{id}</p>
+      {imageLinks.length > 0 &&
+        imageLinks.map((link) => {
+          return (
+            <img key={link} src={link} alt={link} style={{ width: "100px" }} />
+          );
+        })}
     </div>
   );
 }
